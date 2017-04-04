f36eb0aef914118fc8b47bdbafc3988227c06d6d
musembi <musembinzioki@gmail.com>
2017-04-05 00:47:45 +0300

    First iteration done

--- machine-learning/knn/knn.py
+++ machine-learning/knn/knn.py
@@ -1,11 +1,12 @@
 import math
+from collections import Counter
 from .training_vectors import two_atts
 
 """
 Implementation of the k-nearest nodes algorithm
 """
 
-def knn(class_count, sample_data, test):
+def knn(class_count, sample_data, test, label):
     """
     Expects `class_count`, number of possible different classes, as first arg 
     Expects `sample_data` as a list of dicts with as many attributes as desired
@@ -23,15 +24,12 @@ def knn(class_count, sample_data, test):
     # 1. Determine the k-value to use
         # - be an odd number
         # - not be a multiple of number of classes
+        # - equal to or greater than the number of classes
 
-    def find_key():
-        spl = sample_data[0]
-        num = len(spl.keys()) - 1 # minus the label
-        if (num % 2) == 0: # even
-            num += 1 # oddify
-            k = num
-        else:
-            k = num
+    def find_k():
+        k = class_count + 1
+        if (k % 2) == 0:
+        	k += 1
 
         return k
 
@@ -42,9 +40,12 @@ def knn(class_count, sample_data, test):
                 d = d + (el[key] - test[key])**2
             el['distance'] = math.sqrt(d)
 
-        return sample_data
-
-    print euclidean_distance()
+        return sorted(sample_data, key=lambda k: k['distance'])
+    
+    k = find_k()
+    top_k_vals = euclidean_distance()[:k]
+    label = Counter(i['healthy'] for i in top_k_vals).most_common(1)[0][0]
+    return label
 
 
 if __name__ == '__main__':
@@ -52,4 +53,4 @@ if __name__ == '__main__':
         'works_out': 3,
         'eats_vegies': 3
     }
-    knn(5, two_atts, test)
+    print knn(5, two_atts, test, 'healthy')
