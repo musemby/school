5c937580cd204fe7e4b6cc77e9b7717c6da5a9d1
musembi <musembinzioki@gmail.com>
2018-05-22 00:57:13 +0300

    Checkpoint: rsa not quite working

--- network-security/rsa.py
+++ network-security/rsa.py
@@ -8,6 +8,19 @@ import sys
 
 MAX = 1000
 
+
+def _is_prime(x):
+    n = 0
+    if x < 2:
+        return False
+
+    for i in range(2, x//2+1):
+        if x % i == 0:
+            n = n + 1
+
+    return n <= 0
+
+
 def _get_gcd(a, b):
     while b != 0:
         a, b = b, a % b
@@ -29,10 +42,11 @@ def _generate_primes():
 
 
 def _get_single_prime():
-    stop = random.randint(MAX/2, MAX)
-    for x in _generate_primes():
-        if x > stop:
-            return(x)
+    integer = random.randint(2, MAX)
+
+    if not _is_prime(integer):
+        _get_single_prime()
+    return integer
 
 
 def _pick_random_primes():
@@ -55,21 +69,44 @@ def _compute_totient(p, q):
 
 #     return random.choice(primes)
     # return primes[1]
-def get_encryption_key(Tn):
+def _compute_encryption_key(Tn):
     e = random.randrange(1, Tn)
-    gcd = _get_gcd(e, Tn):
+    gcd = _get_gcd(e, Tn)
     while gcd != 1:
         e = random.randrange(1, Tn)
-        gcd = gcd(e, Tn)
+        gcd = _get_gcd(e, Tn)
 
     return e
 
 
-def _compute_decryption_key(e, Tn):
-    for i in range(0, MAX):
-        d = (1 + (i * Tn))/e
-        if d > 0 and isinstance(d, int):
-            return d
+def _compute_decryption_key(e, phi):
+    # for i in range(0, MAX):
+    #     d = (1 + (i * Tn))/e
+    #     if d > 0 and isinstance(d, int):
+    #         return d
+
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+    
+    while e > 0:
+        temp1 = temp_phi/e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+        
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+        
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+    
+    if temp_phi == 1:
+        return d + phi
 
 
 def encrypt(number, e, n):
@@ -80,23 +117,17 @@ def decrypt(cipher, d, n):
     return cipher**d % n
 
 
-class Node():
-
-    def __init__(self, ):
-        pass
-
-    
-
-
 if __name__ == '__main__':
-    p, q = _pick_random_primes()
-    # p, q = 11, 5
+    p = int(input("Enter the first prime number"))
+    q = int(input("Enter the second prime number"))
+
     n = _get_prime_product(p, q)
     Tn = _compute_totient(p, q)
     e = _compute_encryption_key(Tn)
-    d = _compute_decryption_key(e, Tn)
-    cipher = encrypt(2, e, n)
-    data = decrypt(cipher, d, n)
-    print("n: {}, Tn: {}, e: {}, d: {}".format(n, Tn, e, d))
-    print("Encrypted 2 to: {}".format(cipher))
-    print("Encrypted {} to: {}".format(cipher, data))
+    _d = _compute_decryption_key(e, Tn)
+    # import pdb; pdb.set_trace()
+    cipher = encrypt(23242, e, n)
+    data = decrypt(cipher, _d, n)
+    print("n: {}, Tn: {}, e: {}, d: {}".format(n, Tn, e, _d))
+    print("Encrypted {} to: {}".format(data, cipher))
+    print("Decrypted {} to: {}".format(cipher, data))
